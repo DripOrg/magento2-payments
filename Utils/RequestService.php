376cefa38275e72989d31c25325bc5903b313e3e
Andre Campos <andre.tatibano@gmail.com>
2022-09-23 10:04:55 -0300

    fix requestService issue from main host

--- Utils/RequestService.php
+++ Utils/RequestService.php
@@ -165,7 +165,7 @@ class RequestService
         try {
             $this->client->post(self::ERROR_LOGGER, [
                 'json' => [
-                    'website' => get_bloginfo('wpurl'),
+                    'website' => (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]",
                     'ecommerceType' => 'MAGENTO',
                     'pluginVersion' => $this->plugin_version,
                     'error' => $error
